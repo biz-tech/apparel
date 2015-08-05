4ae0c40255cbeba4825939cb8cde5817dee7ea71
biztechhost <brandon@biztechus.com>
2015-08-05 12:05:01 -0700

    Organization 1. list                 O 2. detail               O 3. detail edit          O 4. detail save          O 5. detail delete        X 6. add                  O
    Product
    1. list                 O
    2. detail               O
    3. add cart             X
    
    Order
    1. list                 O
    2. detail               O
    3. detail edit          X
    4. detail save          X
    5. detail send email    X

--- client/js/custom.js
+++ client/js/custom.js
@@ -1,6 +1,6 @@
 var path = "http://crm.biztechus.com";
 var test = "http://172.20.10.184/vtigercrm";
-
+var params = location.search.split('&')[1]; // list 화면에서 클릭한 값을 담아서 detail 조회할때 사용.
 
 // product list
 function cntrProd($scope, $http){ 
@@ -11,25 +11,40 @@ function cntrProd($scope, $http){
 	});
 }
 
-// organization list
-function cntrCont($scope, $http){
-	//var site= path+"/testdata.php?oper=Product";
-	var site= test+"/testdata.php?oper=Contact";	
-	$http.get(site).success(function(response){	
-		$scope.data = response;	
-	});	
-};
-
-// organization detail
-function cntrOrgDetail($scope, $http){
-var params = window.location.search.substr(1).split('&');
-
-var site= test+"/testdata.php?oper=accountDetail&"+params;	
+// product detail
+function cntrProductDetail($scope, $http){
+	//var site= path+"/testdata.php?oper=Product";	
+	var site= test+"/testdata.php?oper=productDetail&"+params;	
 	$http.get(site).success(function(response){
 		$scope.data = response;		
 	}); 
 }
 
+
+// Account list
+function cntrAccount($scope, $http){
+	//var site= path+"/testdata.php?oper=acoount";
+	var site= test+"/testdata.php?oper=acoount";	
+	$http.get(site).success(function(response){	
+		$scope.data = response;	
+	});	
+};
+
+// Account detail
+function cntrAccDetail($scope, $http){
+	//var site= path+"/testdata.php?oper=Product";
+	var site= test+"/testdata.php?oper=accountDetail&"+params;	
+	$http.get(site).success(function(response){
+		$scope.data = response;		
+	}); 
+}
+/*
+vtiger_sobillads for billing information(mandatory)
+vtiger_soshipads for shipping information(mandatory)
+vtiger_salesordercf for salesorderid(mandatory)
+vtiger_inventoryproductrel for product information(mandatory)
+vtiger_inventoryshippingrel for tax information(optional)
+*/
 // order list
 function cntrOrder($scope, $http){
 	//var site= path+"/testdata.php?oper=Product";
@@ -39,3 +54,40 @@ function cntrOrder($scope, $http){
 	});
 }
 
+// order list
+function cntrOrderDetail($scope, $http){
+
+	//var site= path+"/testdata.php?oper=Product";
+	var site= test+"/testdata.php?oper=orderDetail&"+params;		
+	$http.get(site).success(function(response){
+		$scope.data = response;
+	});
+}
+
+ $(function(){
+        $("#edit").click(function(){
+            var $this = $(this);
+            var value = $this.attr('value');		
+			
+            if(value=="Edit"){
+                $this.attr("value","Cancel");
+				document.getElementById("save").style.display = "block";
+            }else{
+                $this.attr("value","Edit");
+            }
+            $(".editable").toggle();
+			//accountid 담기.
+			var accid = $('#originAccid').text();
+			$('input[name="accId"]').val(accid);
+			//현재 url 담기.
+			var url =  window.location.href;			
+			var gourl = url.split('&');				
+			$('input[name="currentUrl"]').val(gourl[0]+"&"+gourl[1]);
+        });		
+        $("input.editable").change(function(){
+            $(this).prev().value($(this).value());
+        });
+		$("#save").click(function(){					
+			document.forms["accEdit"].submit();			
+		});		
+    });
